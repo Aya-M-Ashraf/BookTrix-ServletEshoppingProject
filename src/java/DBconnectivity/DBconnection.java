161fa04dbced41169dab68bfb200cd337d4db5a8
Ahmed Ashraf <Ahmed Ashraf@DESKTOP-JNGM71A>
2016-03-06 19:22:06 +0200

    intial files v2

--- src/java/DBconnectivity/DBconnection.java
+++ src/java/DBconnectivity/DBconnection.java
@@ -1,9 +1,28 @@
 package DBconnectivity;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Ahmed Ashraf
  */
 public class DBconnection {
+     private Connection connection;
+    public DBconnection() throws ClassNotFoundException {
+   
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mydb", "root", "root");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+        }
+    }
     
+    public Connection getConnection(){
+        return connection;
+    }
 }
--- src/java/DBconnectivity/ManipulateDB.java
+++ src/java/DBconnectivity/ManipulateDB.java
@@ -1,9 +1,18 @@
 package DBconnectivity;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Ahmed Ashraf
  */
 public class ManipulateDB {
-    
+    Connection connection;
+
+    public ManipulateDB() throws ClassNotFoundException {
+        DBconnection DBConnection = new DBconnection();
+        connection = DBConnection.getConnection();
+        
+        
+    }
 }
