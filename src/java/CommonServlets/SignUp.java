93e981b6bb3badba31e4fb82f79c1cf1c4ee38f5
hend <>
2016-03-11 13:23:24 +0200

    SignUp Servlet edited - unstable state

--- src/java/CommonServlets/SignUp.java
+++ src/java/CommonServlets/SignUp.java
@@ -1,5 +1,6 @@
 package CommonServlets;
 
+import controllers.ControlServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -9,14 +10,30 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class SignUp extends HttpServlet {
-
+    ControlServlet controlServlet;
+    
+    @Override
+    public void init(){
+        controlServlet = new ControlServlet();
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        PrintWriter out=response.getWriter();
         String type = request.getParameter("type");
         switch (type) {
             case "userName":
-
+            {
+                String userName= request.getParameter("userName");
+                boolean userNameFound = controlServlet.doesUserNameExist(userName);
+                if(userNameFound){
+                    System.out.println("This User Name Alraedy Exists");
+                    out.println("This User Name Alraedy Exists");
+                } else{
+                    System.out.println("valid");
+                    out.println("valid");
+                }  
+            }
                 break;
 
         }
